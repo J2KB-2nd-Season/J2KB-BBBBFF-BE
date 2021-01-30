8bdf1a195c70d152207cf67a9d38148a2458d18d
JooMal <yl95yl@naver.com>
2021-01-30 21:25:47 +0900

    MemberServiceImpl 추가

--- bbbbff/bbbbff/src/main/java/com/j2kb/member/service/MemberService.java
+++ bbbbff/bbbbff/src/main/java/com/j2kb/member/service/MemberService.java
@@ -5,5 +5,5 @@ import com.j2kb.member.vo.MemberVO;
 public interface MemberService {
 	//회원가입 서비스단 메서드
 	public void addMember(MemberVO member) throws Exception;
-	
+	public void isValidatePassword(String memberId, String inputPw);
 }
---
+++ bbbbff/bbbbff/src/main/java/com/j2kb/member/service/MemberServiceImpl.java
@@ -0,0 +1,9 @@
+package com.j2kb.member.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberServiceImpl implements MemberService {
+	
+	
+}
