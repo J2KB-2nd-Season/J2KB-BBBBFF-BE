60c5d8871e4e7671a2f3395549691c145ccfde0a
JooMal <yl95yl@naver.com>
2021-02-01 20:15:55 +0900

    [mybatis] 테스트를 위해 MemberDAO, MemberDAOImpl 수정

--- bbbbff/bbbbff/src/main/java/com/j2kb/member/dao/MemberDAO.java
+++ bbbbff/bbbbff/src/main/java/com/j2kb/member/dao/MemberDAO.java
@@ -1,7 +1,10 @@
 package com.j2kb.member.dao;
 
+import java.util.List;
+
 import com.j2kb.member.vo.MemberVO;
 
 public interface MemberDAO {
 	public void insertNewMember(MemberVO memberVO) throws Exception;
+	public List<MemberVO> selectAllMemberList();
 }
