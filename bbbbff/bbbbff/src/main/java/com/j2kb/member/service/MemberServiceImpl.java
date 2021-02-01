2bc4dd60891bc0126b211a9fe76a793f2a950f8d
takoyummy <takoyummy@naver.com>
2021-02-02 01:09:39 +0900

    MemberServiceImpl에 addMember()추가

--- bbbbff/bbbbff/src/main/java/com/j2kb/member/service/MemberServiceImpl.java
+++ bbbbff/bbbbff/src/main/java/com/j2kb/member/service/MemberServiceImpl.java
@@ -1,20 +1,25 @@
 package com.j2kb.member.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.j2kb.member.dao.MemberDAO;
 import com.j2kb.member.vo.MemberVO;
 
 @Component
 @RequestMapping("/user")
 public class MemberServiceImpl implements MemberService {
-
+	
+	@Autowired
+	private MemberDAO memberDAO;
+	
 	@Override
 	@RequestMapping(value="/join", method=RequestMethod.POST)
 	public void addMember(MemberVO member) throws Exception {
 		// TODO Auto-generated method stub
-		
+		memberDAO.insertNewMember(member);
 	}
 
 	@Override
