8316f50d3d27f25e7fcf669c2b4da1d019e9c004
takoyummy <40001921+takoyummy@users.noreply.github.com>
2021-03-11 16:27:23 +0900

    CartVO클래스 수정
    카멜케이스로 변수 선언

--- src/main/java/com/j2kb/cart/vo/CartVO.java
+++ src/main/java/com/j2kb/cart/vo/CartVO.java
@@ -1,5 +1,62 @@
 package com.j2kb.cart.vo;
 
 public class CartVO {
+	
+	private String cartNum;
+	private String cartQuan;
+	private String prodNum;
+	private String memberId;
+	
+	public CartVO() {
+		
+	}
 
+	public CartVO(String cartNum, String cartQuan, String prodNum, String memberId) {
+		super();
+		this.cartNum = cartNum;
+		this.cartQuan = cartQuan;
+		this.prodNum = prodNum;
+		this.memberId = memberId;
+	}
+
+	public String getCartNum() {
+		return cartNum;
+	}
+
+	public void setCartNum(String cartNum) {
+		this.cartNum = cartNum;
+	}
+
+	public String getCartQuan() {
+		return cartQuan;
+	}
+
+	public void setCartQuan(String cartQuan) {
+		this.cartQuan = cartQuan;
+	}
+
+	public String getProdNum() {
+		return prodNum;
+	}
+
+	public void setProdNum(String prodNum) {
+		this.prodNum = prodNum;
+	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	@Override
+	public String toString() {
+		return "CartVO [cartNum=" + cartNum + ", cartQuan=" + cartQuan + ", prodNum=" + prodNum + ", memberId="
+				+ memberId + "]";
+	}
+	
+	
+	
 }
