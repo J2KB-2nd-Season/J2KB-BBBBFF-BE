e0136f185bd14657abd6945636e934182eaf433f
takoyummy <40001921+takoyummy@users.noreply.github.com>
2021-03-11 19:31:55 +0900

    cart.xml에 resultMap추가

--- src/main/java/com/j2kb/cart/dao/CartDaoImpl.java
+++ src/main/java/com/j2kb/cart/dao/CartDaoImpl.java
@@ -1,5 +1,34 @@
 package com.j2kb.cart.dao;
 
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.j2kb.member.dao.MemberDAOImpl;
+
 public class CartDaoImpl implements CartDao{
+	
+	private static SqlSessionFactory sqlMapper=null;
+	private static final Logger logger = LoggerFactory.getLogger(MemberDAOImpl.class);
+	
+	public static SqlSessionFactory getInstance() {
+		if(sqlMapper==null) {
+			try {
+				String resource = "SqlMapConfig.xml";
+				Reader reader = Resources.getResourceAsReader(resource);
+				sqlMapper = new SqlSessionFactoryBuilder().build(reader);
+				reader.close();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return sqlMapper;
+	}
+	
+	
 
 }
